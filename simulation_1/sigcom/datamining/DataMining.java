fa8662a2c7a699e287a78fe8fbd20c582e3a0650
multilayerSocailGraph <joseph.chaozhang@gmail.com>
2014-06-25 13:14:07 -0400

    changed configuration files

--- simulation_1/sigcom/datamining/DataMining.java
+++ simulation_1/sigcom/datamining/DataMining.java
@@ -2,6 +2,7 @@ package sigcom.datamining;
 
 public class DataMining {
 	public static void dataProcess(int dataProcessFlag) {
+<<<<<<< HEAD
 		
 		// Process raw data files in RawData folder
 		//processRawdata();
@@ -11,10 +12,24 @@ public class DataMining {
 		
 		// Process data files in stage_1 folder in new format
 		processStage1data2();	
+=======
+>>>>>>> ChaoBranch
 
+		switch (dataProcessFlag) {
+		case 0:
+			processRawdata(); // Process raw data files in RawData folder
+			break;
+		case 1:
+			// Process data files in stage_1 folder
+			processStage1data2();
+			break;
+		default:
+			processRawdata();
+			processStage1data2();
+			break;
+		}
 	}
-	
-	
+
 	/*********************************************
 	 * 
 	 * Process data files in stage_1 folder to new format
@@ -23,16 +38,19 @@ public class DataMining {
 	private static void processStage1data2() {
 		Stage1Process3 stage1_dataset3 = new Stage1Process3("./Stage_1");
 		stage1_dataset3.combineRelationships();
-		
+
 		System.out.println("Stage 1 Dataset process 3 is done.");
-		
+
 		Stage1Process4 stage1_dataset4 = new Stage1Process4("./Stage_1");
 		stage1_dataset4.combineRelationships();
-		
-		System.out.println("Stage 1 Dataset process 4 is done.");	
-		
+
+<<<<<<< HEAD
+=======
+		System.out.println("Stage 1 Dataset process 4 is done.");
+
 	}
 
+>>>>>>> ChaoBranch
 	/*********************************************
 	 * 
 	 * Process raw data files in RawData folder
@@ -42,29 +60,29 @@ public class DataMining {
 		RawDataProcess raw_datasets = new RawDataProcess("./RawData");
 		// change activity file
 		raw_datasets.activity_processs();
-		
+
 		// Merge friends1 and friends2 file
 		raw_datasets.mergeFriends();
-		
+
 		// Merge interets1 and interets2 file
 		raw_datasets.mergeInterests();
-		
+
 		// Remove redundant informations from files
 		// messages,receptions,transmissions
 		raw_datasets.messagesProce();
-		
+
 		// Change format of participants
 		raw_datasets.participantsProce();
-		
+
 		// Remove users who are not participated
 		raw_datasets.proximityProce();
-		
+
 		// Remove message id
 		raw_datasets.receptionProce();
-		
-		// Remove message id and success status
-		raw_datasets.transmissionProce();		
 
-		System.out.println("Raw Data process is done.");		
+		// Remove message id and success status
+		raw_datasets.transmissionProce();
+
+		System.out.println("Raw Data process is done.");
 	}
 }
