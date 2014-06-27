f14ee0a387dc479642f2866b78e9c7262b0ca4b5
multilayerSocailGraph <joseph.chaozhang@gmail.com>
2014-06-27 10:39:25 -0400

    Change interest to binay
    Changes are made in Stage1Data_discretize.java

--- simulation_1/sigcom/datamining/Stage1Data_discretize.java
+++ simulation_1/sigcom/datamining/Stage1Data_discretize.java
@@ -35,7 +35,7 @@ public class Stage1Data_discretize extends PreprocessBase {
 		 */
 		ArrayList<Attribute> atts = new ArrayList<Attribute>();
 		atts.add(new Attribute("senderId"));
-		atts.add(new Attribute("recieverId"));	
+		atts.add(new Attribute("recieverId"));
 		atts.add(new Attribute("same_institution"));
 		atts.add(new Attribute("same_city"));
 		atts.add(new Attribute("same_country"));
@@ -195,8 +195,8 @@ public class Stage1Data_discretize extends PreprocessBase {
 						}
 					}
 				}
-				vals[19] = commonInterest_count;
-				
+				vals[19] = (commonInterest_count > 0)? 1 : 0;
+								
 				DenseInstance newIns = new DenseInstance(1.0, vals);				
 				firstFriendship.add(newIns);
 			}
@@ -206,7 +206,7 @@ public class Stage1Data_discretize extends PreprocessBase {
 		NumericToNominal convert = new NumericToNominal();
 		String[] options = new String[2];
 		options[0] = "-R";
-		options[1] = "3, 4, 5, 21"; // range of variables to make numeric		
+		options[1] = "3, 4, 5, 20, 21"; // range of variables to make nominal		
 
 		try {
 			// Convert numeric attribute to nominal attribute
@@ -232,7 +232,7 @@ public class Stage1Data_discretize extends PreprocessBase {
 		discreteptions[3] = "-M"; // Specifies the desired weight of instances per bin for equal-frequency binning.
 		discreteptions[4] = "-1"; // default
 		discreteptions[5] = "-R"; // Attribute index
-		discreteptions[6] = "6-20"; // the second attribute
+		discreteptions[6] = "6-19"; // the second attribute
 		
 		try {
 			// Discrete data size and convert to ordinal attribute
@@ -245,7 +245,7 @@ public class Stage1Data_discretize extends PreprocessBase {
 				for (int n = 0; n < att.numValues(); n++) {
 					firstFriendship.renameAttributeValue(att, att.value(n), "" + n);
 				}
-			}			
+			}
 			
 			// Save modified data with discretized duration attribute
 			ArffSaver saver = new ArffSaver();
