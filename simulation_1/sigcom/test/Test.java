b84c893e808287c49012c0829f77147551984783
multilayerSocailGraph <joseph.chaozhang@gmail.com>
2014-07-15 15:49:00 +0800

    nothing changed

--- simulation_1/sigcom/test/Test.java
+++ simulation_1/sigcom/test/Test.java
@@ -4,11 +4,13 @@ public class Test {
 
 	public static void main(String[] args) 
 	{
+		int j = 0;
 		// TODO Auto-generated method stub
-		for(int i = 80000; i<=200000; i+=3600)
+		for(int i = 80000; i<=250000; i+=3600)
 		{
 			System.out.print(i+",");
 		}
+		
 
 	}
 
